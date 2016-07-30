7b2050a3373025a277ac80e701d6e51792ccdfcb
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2016-07-31 01:06:14 +0200

    Openldap: Fixed bug, when no SLAPD_* variables are defined. Openldap: Changed bash to be more strict PHPLDAPAdmin: Changed bash to be more strict

--- openldap/entrypoint.sh
+++ openldap/entrypoint.sh
@@ -1,138 +1,142 @@
 #!/bin/sh
 
+# Bash "strict mode"
+# Exit if any return value is non zero (-e), any variables are not reclared (-u)
+# and do not mask errors in pipelines (using |) (-o pipefail)
+set -euo pipefail
+
 # When not limiting the open file descritors limit, the memory consumption of
 # slapd is absurdly high. See https://github.com/docker/docker/issues/8231
 ulimit -n 8192
 
-set -e
-  # Run this on first start
-  # Make sure the config folder does *not* exist or is empty
-  if [[ ! -d "/etc/openldap/slapd.d" ]] || [ -z "$(ls -A /etc/openldap/slapd.d)" ]; then
-    # First do some sanity checks
-    if [[ -z "$SLAPD_PASSWORD" ]]; then
-      echo -n >&2 "Error: Container not configured and SLAPD_PASSWORD not set. "
-      echo >&2 "Did you forget to add -e SLAPD_PASSWORD=... ?"
-      exit 1
-    fi
+# Run this on first start
+# Make sure the config folder does *not* exist or is empty
+if [[ ! -d "/etc/openldap/slapd.d" ]] || [ -z "$(ls -A /etc/openldap/slapd.d)" ]; then
+  # First do some sanity checks
+  if [[ -z "$SLAPD_PASSWORD" ]]; then
+    echo -n >&2 "Error: Container not configured and SLAPD_PASSWORD not set. "
+    echo >&2 "Did you forget to add -e SLAPD_PASSWORD=... ?"
+    exit 1
+  fi
 
-    if [[ -z "$SLAPD_DOMAIN" ]]; then
-      echo -n >&2 "Error: Container not configured and SLAPD_DOMAIN not set. "
-      echo >&2 "Did you forget to add -e SLAPD_DOMAIN=... ?"
-      exit 1
-    fi
+  if [[ -z "$SLAPD_DOMAIN" ]]; then
+    echo -n >&2 "Error: Container not configured and SLAPD_DOMAIN not set. "
+    echo >&2 "Did you forget to add -e SLAPD_DOMAIN=... ?"
+    exit 1
+  fi
 
-    # Check if there is an existing database but without a config
-    # Exclude DB_CONFIG.example from the list of files, because it was installed by the package.
-    if [ -n "$(ls -A /var/lib/openldap/openldap-data | egrep -v '^DB_CONFIG.example$')" ]; then
-      echo 'Error: Existing database found, but no config was found. Restore the config and import from backup!'
-      echo -e "Data directory contains: \n$(ls -A /var/lib/openldap/openldap-data | egrep -v '^DB_CONFIG.example$')"
-      exit 1
-    fi
+  # Check if there is an existing database but without a config
+  # Exclude DB_CONFIG.example from the list of files, because it was installed by the package.
+  if [ -n "$(ls -A /var/lib/openldap/openldap-data | egrep -v '^DB_CONFIG.example$')" ]; then
+    echo 'Error: Existing database found, but no config was found. Restore the config and import from backup!'
+    echo -e "Data directory contains: \n$(ls -A /var/lib/openldap/openldap-data | egrep -v '^DB_CONFIG.example$')"
+    exit 1
+  fi
 
-    # Create a config folder
-    mkdir -p /etc/openldap/slapd.d
+  # Create a config folder
+  mkdir -p /etc/openldap/slapd.d
 
-    # Add additional schemas
-    if [ -n "$SLAPD_ADDITIONAL_SCHEMAS" ]; then
-        IFS=','
-        schemas=$SLAPD_ADDITIONAL_SCHEMAS
-        for schema in ${schemas}; do
-          schema="${schema}.schema"
-          schema_file="/etc/openldap/schema/${schema}"
-          echo "Adding schema ${schema} to database..."
-          if [ -e "${schema_file}" ]; then
-            sed -i "/nis.schema/a include		${schema_file}" /etc/openldap/slapd.conf
-          else
-            echo -n >&2 "Error: Cannot find schema ${schema_file}. "
-            echo >&2 "Did you forget to copy it to schema/ ?"
-            exit 1
-          fi
-        done
-        unset IFS
-    fi
-
-    # Create a secure password hash for the root account
-    # We use standard Linux CRYPT hashes
-    password_hash=`slappasswd -h {CRYPT} -c '$6$%.16s' -s "${SLAPD_PASSWORD}"`
-    # Escape the hash for use with sed
-    password_hash=${password_hash//\//\\\/}
-    # Insert the password into the config file
-    sed -i "s|rootpw.*|rootpw		${password_hash}|g" /etc/openldap/slapd.conf
-
-    # Generate the base string from the domain
-    dc_string=''
-    IFS='.'
-    odc=''
-    dc_parts=$SLAPD_DOMAIN
-    for dc_part in ${dc_parts}; do
-      [ -z "${dc_string}" ] && odc="${dc_part}"
-        dc_string="${dc_string},dc=${dc_part}"
+  # Add additional schemas
+  if [ -n "$SLAPD_ADDITIONAL_SCHEMAS" ]; then
+    IFS=','
+    schemas=$SLAPD_ADDITIONAL_SCHEMAS
+    for schema in ${schemas}; do
+      schema="${schema}.schema"
+      schema_file="/etc/openldap/schema/${schema}"
+      echo "Adding schema ${schema} to database..."
+      if [ -e "${schema_file}" ]; then
+        sed -i "/nis.schema/a include		${schema_file}" /etc/openldap/slapd.conf
+      else
+        echo -n >&2 "Error: Cannot find schema ${schema_file}. "
+        echo >&2 "Did you forget to copy it to schema/ ?"
+        exit 1
+      fi
     done
     unset IFS
-    # Remove leading ','
-    base_string="${dc_string:1}"
+  fi
 
-    # Append domain name to the organization
-    SLAPD_ORGANIZATION="${SLAPD_ORGANIZATION:-${SLAPD_DOMAIN}}"
+  # Create a secure password hash for the root account
+  # We use standard Linux CRYPT hashes
+  password_hash=`slappasswd -h {CRYPT} -c '$6$%.16s' -s "${SLAPD_PASSWORD}"`
+  # Escape the hash for use with sed
+  password_hash=${password_hash//\//\\\/}
+  # Insert the password into the config file
+  sed -i "s|rootpw.*|rootpw		${password_hash}|g" /etc/openldap/slapd.conf
 
-    # Configure base directory
-    sed -i "s|dc=example,dc=net|$base_string|g" /etc/openldap/slapd.conf
-    sed -i "s|dc=example,dc=net|$base_string|g" /etc/openldap/modules/base.ldif
-    sed -i "s|dc: example|dc: $odc|g" /etc/openldap/modules/base.ldif
-    sed -i "s|o: Example|o: $SLAPD_ORGANIZATION|g" /etc/openldap/modules/base.ldif
-    sed -i "s|description: My LDAP Server|description: $SLAPD_DESCRIPTION|g" /etc/openldap/modules/base.ldif
+  # Generate the base string from the domain
+  dc_string=''
+  IFS='.'
+  odc=''
+  dc_parts=$SLAPD_DOMAIN
+  for dc_part in ${dc_parts}; do
+    [ -z "${dc_string}" ] && odc="${dc_part}"
+      dc_string="${dc_string},dc=${dc_part}"
+  done
+  unset IFS
+  # Remove leading ','
+  base_string="${dc_string:1}"
 
-    # Create a new database file
-    echo -n 'Data directory is empty, generating database...'
-    cp /var/lib/openldap/openldap-data/DB_CONFIG.example /var/lib/openldap/openldap-data/DB_CONFIG
-    chown -R ldap:ldap /var/lib/openldap/openldap-data
-    chmod 700 /var/lib/openldap/openldap-data
+  # Append domain name to the organization
+  SLAPD_ORGANIZATION="${SLAPD_ORGANIZATION:-${SLAPD_DOMAIN}}"
 
-    # Run slapd once to generate a database
-    slapd -u ldap -g ldap >/dev/null 2>&1
-    killall slapd
-    echo 'Done.'
+  # Configure base directory
+  sed -i "s|dc=example,dc=net|$base_string|g" /etc/openldap/slapd.conf
+  sed -i "s|dc=example,dc=net|$base_string|g" /etc/openldap/modules/base.ldif
+  sed -i "s|dc: example|dc: $odc|g" /etc/openldap/modules/base.ldif
+  sed -i "s|o: Example|o: $SLAPD_ORGANIZATION|g" /etc/openldap/modules/base.ldif
+  sed -i "s|description: My LDAP Server|description: $SLAPD_DESCRIPTION|g" /etc/openldap/modules/base.ldif
 
-    # Since Openldap version 2.3 all settings are stored in an online configuration
-    # which is stored in slapd.d/. Since it is much easier to configure
-    # the old config file slapd.conf, we use slaptest to convert the file
-    slaptest -f /etc/openldap/slapd.conf -F /etc/openldap/slapd.d/
-    # Add root nodes
-    echo 'Adding root nodes...'
-    # No need to add -F, beause we have set this in OPTS
-    slapadd -v -l /etc/openldap/modules/base.ldif
+  # Create a new database file
+  echo -n 'Data directory is empty, generating database...'
+  cp /var/lib/openldap/openldap-data/DB_CONFIG.example /var/lib/openldap/openldap-data/DB_CONFIG
+  chown -R ldap:ldap /var/lib/openldap/openldap-data
+  chmod 700 /var/lib/openldap/openldap-data
 
-    chown -R ldap:ldap /etc/openldap/slapd.d
-    su ldap -s /bin/sh -c slapindex
+  # Run slapd once to generate a database
+  slapd -u ldap -g ldap >/dev/null 2>&1
+  killall slapd
+  echo 'Done.'
 
-    if [[ -n "$SLAPD_ADDITIONAL_MODULES" ]]; then
-      IFS=","
-      modules=$SLAPD_ADDITIONAL_MODULES;
+  # Since Openldap version 2.3 all settings are stored in an online configuration
+  # which is stored in slapd.d/. Since it is much easier to configure
+  # the old config file slapd.conf, we use slaptest to convert the file
+  slaptest -f /etc/openldap/slapd.conf -F /etc/openldap/slapd.d/
+  # Add root nodes
+  echo 'Adding root nodes...'
+  # No need to add -F, beause we have set this in OPTS
+  slapadd -v -l /etc/openldap/modules/base.ldif
 
-      for module in "${modules}"; do
-        module_file="/etc/openldap/modules/${module}.ldif"
-        echo "Adding module ${module} to database..."
-        if [ -e "${module_file}" ]; then
-          # No need to add -F, beause we have set this in OPTS
-          slapadd -n0 -l "${module_file}"
-        else
-          echo -n >&2 "Error: Cannot find module ${module_file}. "
-          echo >&2 "Did you forget to copy it to modules/ ?"
-          exit 1
-        fi
-      done
-      unset IFS
-    fi
+  chown -R ldap:ldap /etc/openldap/slapd.d
+  su ldap -s /bin/sh -c slapindex
+
+  if [[ -n "$SLAPD_ADDITIONAL_MODULES" ]]; then
+    IFS=","
+    modules=$SLAPD_ADDITIONAL_MODULES;
+
+    for module in "${modules}"; do
+      module_file="/etc/openldap/modules/${module}.ldif"
+      echo "Adding module ${module} to database..."
+      if [ -e "${module_file}" ]; then
+        # No need to add -F, beause we have set this in OPTS
+        slapadd -n0 -l "${module_file}"
+      else
+        echo -n >&2 "Error: Cannot find module ${module_file}. "
+        echo >&2 "Did you forget to copy it to modules/ ?"
+        exit 1
+      fi
+    done
+    unset IFS
+  fi
 
   # Your LDAP directory is now ready to be populated
   echo "Finished setting up Openldap."
 
-  else
-    slapd_configs_in_env=`env | grep 'SLAPD_'`
+else
+  slapd_configs_in_env=$(env | grep 'SLAPD_' || true)
 
-    if [ -n "${slapd_configs_in_env:+x}" ]; then
-        echo "Info: Container already configured, for security reasons remove the SLAPD_* environment variables"
-    fi
+  if [ -n "${slapd_configs_in_env}" ]; then
+    echo "WARN Container already configured, for security reasons remove the SLAPD_* environment variables"
+  fi
 fi
 
 exec "$@"
--- phpldapadmin/entrypoint.sh
+++ phpldapadmin/entrypoint.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
+# Bash "strict mode"
+# Exit if any return value is non zero (-e), any variables are not reclared (-u)
+# and do not mask errors in pipelines (using |) (-o pipefail)
+set -euo pipefail
+
 # Set dfault variables
 SERVER_DESCRIPTION="${SERVER_DESCRIPTION:-My LDAP Server}"
 LDAP_SERVER="${LDAP_SERVER:-openldap}"
 LDAP_PORT="${LDAP_PORT:-389}"
 LDAP_BIND_DN="${LDAP_BIND_DN:-cn=root,dc=example,dc=net}"
 
-set -e
+sed -i "s|'server','name','My LDAP Server'|'server','name','${SERVER_DESCRIPTION}'|g" /usr/share/webapps/phpldapadmin/config/config.php
+sed -i "s|'server','host','127.0.0.1'|'server','host','${LDAP_SERVER}'|g" /usr/share/webapps/phpldapadmin/config/config.php
+sed -i "s|'server','port',389|'server','port',${LDAP_PORT}|g" /usr/share/webapps/phpldapadmin/config/config.php
+sed -i "s|'login','bind_id','cn=Manager,dc=example,dc=com'|'login','bind_id','${LDAP_BIND_DN}'|g" /usr/share/webapps/phpldapadmin/config/config.php
 
-  sed -i "s|'server','name','My LDAP Server'|'server','name','${SERVER_DESCRIPTION}'|g" /usr/share/webapps/phpldapadmin/config/config.php
-  sed -i "s|'server','host','127.0.0.1'|'server','host','${LDAP_SERVER}'|g" /usr/share/webapps/phpldapadmin/config/config.php
-  sed -i "s|'server','port',389|'server','port',${LDAP_PORT}|g" /usr/share/webapps/phpldapadmin/config/config.php
-  sed -i "s|'login','bind_id','cn=Manager,dc=example,dc=com'|'login','bind_id','${LDAP_BIND_DN}'|g" /usr/share/webapps/phpldapadmin/config/config.php
-
-  # Remove the Sourceforge logo in the lower right corner, because we don't want external 3rd party content in our local network
-  # We do this by removing a case block from functions.php. The 'logo' block will be cut out using sed
-  sed -i "/case 'logo':/,/return/d" /usr/share/webapps/phpldapadmin/lib/functions.php
+# Remove the Sourceforge logo in the lower right corner, because we don't want external 3rd party content in our local network
+# We do this by removing a case block from functions.php. The 'logo' block will be cut out using sed
+sed -i "/case 'logo':/,/return/d" /usr/share/webapps/phpldapadmin/lib/functions.php
 
 exec "$@"
