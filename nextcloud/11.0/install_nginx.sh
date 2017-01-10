a7192315ee89a121b1e77eee50b947c8b3fa95a2
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2017-01-10 02:12:44 +0100

    We can now change UID and GID of the php/nginx instance

--- nextcloud/11.0/install_nginx.sh
+++ nextcloud/11.0/install_nginx.sh
@@ -8,9 +8,24 @@ set -euo pipefail
 # To colour the bash use the following command:
 # echo -e "${COLOUR}foo\e[0m"
 COLOUR='\e[1;93m'
+# THe ids used by nginx, this will be replaced by the ones set in the Dockerfile
+OLD_UID=101
+OLD_GID=101
+USER="nginx"
+USER_GROUP=${USER}
 
 echo -ne "${COLOUR}Installing Nginx...\e[0m"
 apk add -U nginx
+
+# Change user/group to UID and GID set in the Dockerfile
+echo -e "${COLOUR}Changing user and group...\e[0m"
+deluser nginx
+addgroup -g ${GID} ${USER_GROUP}
+adduser -D -S -u ${UID} -h /var/lib/nginx -s /sbin/nologin -g ${USER} -G ${USER_GROUP} ${USER}
+
+echo -e "${COLOUR}Changing owner and permissions for old uid/gid files...\e[0m"
+find / -user ${OLD_UID} -exec chown ${USER} {} \;
+find / -group ${OLD_GID} -exec chgrp ${USER_GROUP} {} \;
 
 # The latest version of nginx stores its pid in /run/nginx/
 mkdir -p /run/nginx
