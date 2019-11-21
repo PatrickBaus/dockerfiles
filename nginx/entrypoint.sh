91966d2f849fec41b021d73838d26fbf1ee9f075
Patrick Baus <patrick.baus@physik.tu-damrstadt.de>
2019-11-21 09:34:05 +0100

    Updated entrypoint to conditionally treat the nginx certificates

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -5,7 +5,6 @@
 # and do not mask errors in pipelines (using |) (-o pipefail)
 set -euo pipefail
 
-chmod 700 /etc/nginx/certs
-chmod 600 /etc/nginx/certs/*
+[ -d /etc/nginx/certs ] && chmod 700 /etc/nginx/certs && chmod 600 /etc/nginx/certs/*
 
 exec "$@"
