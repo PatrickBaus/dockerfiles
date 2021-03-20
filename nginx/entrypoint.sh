bea812907bb831778e69d561cb732d4dd45096bd
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2021-03-20 05:59:37 +0100

    Updated nginx to 1.19.8 Added static signing keys for nginx and libressl

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -5,6 +5,4 @@
 # and do not mask errors in pipelines (using |) (-o pipefail)
 set -euo pipefail
 
-[ -d /etc/nginx/certs ] && chmod 700 /etc/nginx/certs && chmod 600 /etc/nginx/certs/*
-
 exec "$@"
