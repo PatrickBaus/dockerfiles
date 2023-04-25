35b80994d3a605580a3987b029f492c4d6164f8d
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2023-04-26 00:21:20 +0200

    Updated Nextcloud to 26.0.1

--- nextcloud/26.0/upgrade_nextcloud.sh
+++ nextcloud/26.0/upgrade_nextcloud.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-su -s /bin/sh -c 'php8 /var/www/nextcloud/occ upgrade' nginx
-su -s /bin/sh -c 'php8 /var/www/nextcloud/occ db:add-missing-indices' nginx
-su -s /bin/sh -c 'php8 /var/www/nextcloud/occ db:convert-filecache-bigint' nginx
+su -s /bin/sh -c 'php81 /var/www/nextcloud/occ upgrade' nginx
+su -s /bin/sh -c 'php81 /var/www/nextcloud/occ db:add-missing-indices' nginx
+su -s /bin/sh -c 'php81 /var/www/nextcloud/occ db:convert-filecache-bigint' nginx
