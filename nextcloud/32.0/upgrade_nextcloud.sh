c93cb361619c3765d6fd2a38047ec4661b395968
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2026-01-16 11:10:50 +0100

    Change upgrade script to PHP 8.4

--- nextcloud/32.0/upgrade_nextcloud.sh
+++ nextcloud/32.0/upgrade_nextcloud.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-su -s /bin/sh -c 'php83 /var/www/nextcloud/occ upgrade' nginx
-su -s /bin/sh -c 'php83 /var/www/nextcloud/occ db:add-missing-indices' nginx
-su -s /bin/sh -c 'php83 /var/www/nextcloud/occ db:convert-filecache-bigint' nginx
+su -s /bin/sh -c 'php84 /var/www/nextcloud/occ upgrade' nginx
+su -s /bin/sh -c 'php84 /var/www/nextcloud/occ db:add-missing-indices' nginx
+su -s /bin/sh -c 'php84 /var/www/nextcloud/occ db:convert-filecache-bigint' nginx
